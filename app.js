e33a6bb5927ac20a6d4098719d6a0eba25265771
Gisela-Camacho-H <gis25cam@gmail.com>
2021-11-18 20:32:02 -0600

    add port

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ app.use(express.static(publicPath));
 
 const port = 3000
 
-app.listen(port, () => console.log(`Servidor corriendo en puerto ${port}`));
+app.listen(process.env.PORT || port, () => console.log(`Servidor corriendo en puerto ${port}`));
 
 app.get('/', (req, res) => {
     res.sendFile(path.resolve(__dirname, './views/home.html'));
